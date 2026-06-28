ce30d20ba4c25155d0333437add0985b84a955a8
Mateus Rodrigues de Morais <mateus@mateus.tech>
2026-06-27 21:58:38 -0300

    Update base path in Vite configuration to '/explore-website/'

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import path from 'path'
 
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-  base: '/explore-landing/',
+  base: '/explore-website/',
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src'),
